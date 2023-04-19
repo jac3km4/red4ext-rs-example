133b79852bbfd683e7346f6d3cc1a15569e57251
Roms1383 <romain.kelifa@gmail.com>
2023-04-19 12:01:27 +0700

    Add TDBID test

--- reds/example.reds
+++ reds/example.reds
@@ -1,2 +1,4 @@
 
 native func SumInts(ints: array<Int32>) -> Int32;
+native func PluginName() -> String;
+native func Convert(name: String) -> TweakDBID;
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use red4ext_rs::prelude::*;
+use red4ext_rs::{prelude::*, interop::TweakDBID};
 
 define_plugin! {
     name: "example",
@@ -6,9 +6,19 @@ define_plugin! {
     version: 1:0:0,
     on_register: {
         register_function!("SumInts", sum_ints);
+        register_function!("PluginName", plugin_name);
+        register_function!("Convert", convert);
     }
 }
 
 fn sum_ints(ints: Vec<i32>) -> i32 {
     ints.iter().sum()
+}
+
+fn plugin_name() -> String {
+    String::from("RED4EXT.RS.EXAMPLE")
+}
+
+fn convert(name: String) -> TweakDBID {
+    TweakDBID::new(&name)
 }
