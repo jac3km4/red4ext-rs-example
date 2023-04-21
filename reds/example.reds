5c7b862c245fb653fa6e875c4b7c2f4c425f6e3b
Rom's <romain.kelifa@gmail.com>
2023-04-21 18:36:53 +0700

    Support for TDBID (#1)
    * Add TDBID test
    
    * Add test
    
    * Update Red4Ext dep
    
    * Configure Git line endings
    
    * Update from latest changes
    
    * Test new from base
    
    * Add comment
    
    * Update upstream
    
    * Import from prelude
    
    * Update upstream
    
    * Update upstream
    
    * Fix import
    
    Co-authored-by: jekky <11986158+jac3km4@users.noreply.github.com>
    
    * Update upstream
    
    * Rename from code review
    
    Co-authored-by: jekky <11986158+jac3km4@users.noreply.github.com>
    
    * Rename from code review
    
    Co-authored-by: jekky <11986158+jac3km4@users.noreply.github.com>
    
    * Remove unused dummy method
    
    Co-authored-by: jekky <11986158+jac3km4@users.noreply.github.com>
    
    * Update docs
    
    * Update from tagged upstream
    
    ---------
    
    Co-authored-by: jekky <11986158+jac3km4@users.noreply.github.com>

--- reds/example.reds
+++ reds/example.reds
@@ -1,2 +1,5 @@
 
 native func SumInts(ints: array<Int32>) -> Int32;
+native func PluginName() -> String;
+native func CreateTweakDBID(name: String) -> TweakDBID;
+native func AppendToTweakDBID(base: TweakDBID, suffix: String) -> TweakDBID;
--- src/lib.rs
+++ src/lib.rs
@@ -6,9 +6,49 @@ define_plugin! {
     version: 1:0:0,
     on_register: {
         register_function!("SumInts", sum_ints);
+        register_function!("PluginName", plugin_name);
+        register_function!("CreateTweakDBID", create_tweakdb_id);
+        register_function!("AppendToTweakDBID", append_to_tweakdb_id);
     }
 }
 
+/// SumInts
+/// 
+/// test in CET like:
+/// ```swift
+/// LogChannel(CName.new("DEBUG"), ToString(SumInts([2000, 77])));
+/// ```
 fn sum_ints(ints: Vec<i32>) -> i32 {
     ints.iter().sum()
+}
+
+/// PluginName
+/// 
+/// test in CET like:
+/// ```swift
+/// LogChannel(CName.new("DEBUG"), PluginName());
+/// ```
+fn plugin_name() -> String {
+    String::from("RED4EXT.RS.EXAMPLE")
+}
+
+/// CreateTweakDBID
+/// 
+/// test in CET like:
+/// ```swift
+/// LogChannel(CName.new("DEBUG"), CreateTweakDBID("A.Test"));
+/// ```
+fn create_tweakdb_id(name: String) -> TweakDBID {
+    TweakDBID::new(&name)
+}
+
+
+/// AppendToTweakDBID
+/// 
+/// test in CET like:
+/// ```swift
+/// LogChannel(CName.new("DEBUG"), TDBID.Create("A.Test") == AppendToTweakDBID(TDBID.Create("A."), "Test"));
+/// ```
+fn append_to_tweakdb_id(base: TweakDBID, suffix: String) -> TweakDBID {
+    TweakDBID::new_from_base(&base, suffix.as_str())
 }
